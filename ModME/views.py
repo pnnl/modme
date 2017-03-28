45c571456690185532895a1b46e33f41b358e21a
Leif Carlsen <leif.carlsen@pnnl.gov>
2017-03-28 09:54:30 -0700

    refactor:rename variable

--- ModME/views.py
+++ ModME/views.py
@@ -207,7 +207,7 @@ def new(request):
 
 def complete(request):
     data = json.loads(request.POST.get('data'))
-    condId = request.POST.get('id')
+    conditionID = request.POST.get('id')
     metadata = Metadata(
         startTime=data[0]["time"],
         sessionID=data[0]["sessionID"],
@@ -308,17 +308,17 @@ def complete(request):
 
 ######SaveUniqueString3m3n4h5g6c6c3n3id8fg7o3n033hn########
 
-    print Condition.objects.get(pk=condId)
-    print Condition.objects.get(pk=condId).surveys.all()[0].fileName
+    print Condition.objects.get(pk=conditionID)
+    print Condition.objects.get(pk=conditionID).surveys.all()[0].fileName
 
-    if len(Condition.objects.get(pk=condId).surveys.all()) == 0:
+    if len(Condition.objects.get(pk=conditionID).surveys.all()) == 0:
         return render(request, 'ModME/complete.html')
     else:
         requiredFiles = ["d3/d3.v3.min.js", "d3/d3.chart.min.js"]
-        for j in Condition.objects.get(pk=condId).surveys.all()[0].surveyfile_set.all():
+        for j in Condition.objects.get(pk=conditionID).surveys.all()[0].surveyfile_set.all():
             if j.name not in requiredFiles:
                 requiredFiles.append(j.name)
-        return render(request, 'ModME/survey.html', {'condition': Condition.objects.get(pk=condId), 'ind': 0, 'fileList': requiredFiles, "sessionID": data[0]["sessionID"], 'survey': Condition.objects.get(pk=condId).surveys.all()[0].fileName})
+        return render(request, 'ModME/survey.html', {'condition': Condition.objects.get(pk=conditionID), 'ind': 0, 'fileList': requiredFiles, "sessionID": data[0]["sessionID"], 'survey': Condition.objects.get(pk=conditionID).surveys.all()[0].fileName})
 
 
 def survey(request):
