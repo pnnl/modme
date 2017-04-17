ee1d2268981cb4d41f738de4deeb78f0e79bb11e
Leif Carlsen <leif.carlsen@pnnl.gov>
2017-04-17 02:47:09 -0700

    cosmetic: pep-8 formatting

--- ModME/services.py
+++ ModME/services.py
@@ -1,21 +1,22 @@
 from django.http import HttpResponse
 from django.core import serializers
 from ModME.models import (
-    Condition,
     Metadata,
     Event,
     Session
 )
 import json
 
-# Sorry; serializers.serialize doesn't go deep enough into the tree (doesn't serialize the session object) and json.dumps by default says metadata is not serializable
-# This avoids bringing in another library, though we could... but the last one that went deep enough without us writing anything is apparently now not maintained.
+
 def serialize(obj):
+    # Sorry; serializers.serialize doesn't go deep enough into the tree (doesn't serialize the session object) and
+    # json.dumps by default says metadata is not serializable.  This avoids bringing in another library, though we
+    # could... but the last one that went deep enough without us writing anything is apparently now not maintained.
     if isinstance(obj, Metadata):
         return {
             'id': obj.id,
             'allowEventReuse': obj.allowEventReuse,
-            'condition_id': obj.condition_id, # Not serializing condition here... tree gets large.
+            'condition_id': obj.condition_id,  # Not serializing condition here... tree gets large.
             'session': serialize(obj.session),
             'startTime': obj.startTime,
             'participant_id': obj.participant_id,
@@ -29,23 +30,24 @@ def serialize(obj):
     # Ugly. Eh.
     return obj.__dict__
 
+
 def getReusableSessions(request):
     metadata = '[]'
     condition = request.GET.get('condition')
-    
+
     if condition:
         metadata = Metadata.objects.filter(condition=condition, allowEventReuse=True)
         metadata = json.dumps(list(metadata), default=serialize)
-    
+
     return HttpResponse(metadata, content_type='application/json')
+
 
 def getEventsForMetadata(request):
     events = '[]'
     metadata = request.GET.get('metadata')
-    
+
     if metadata:
         events = Event.objects.filter(metadata=metadata)
         events = serializers.serialize('json', events)
-    
-    return HttpResponse(events, content_type='application/json')
 
+    return HttpResponse(events, content_type='application/json')
