88c3df8a3abd31af371431a98a666e06f7d6934b
Leif Carlsen <leif.carlsen@pnnl.gov>
2017-04-17 01:36:27 -0700

    MODME-6 limit scope of activity initialization blocks by wrapping them in anonymous functions

--- static/ModME/monitoring.js
+++ static/ModME/monitoring.js
@@ -1,37 +1,40 @@
-var monitor_data = setup.Monitoring.data;
-var monitor_param = monitor_data.parameters;
-
-monitor_svg = GUIUtil.getGenericSVG(
-    d3.select("#"+setup.Monitoring.container),
-    650, 650,
-    JSON.parse(document.getElementById(setup.Monitoring.container).style.width.substr(0,document.getElementById(setup.Monitoring.container).style.width.length-2)), 
-    JSON.parse(document.getElementById(setup.Monitoring.container).style.height.substr(0,document.getElementById(setup.Monitoring.container).style.height.length-2)),
-    {top: scale(.03), right: scale(.03), bottom: scale(.03), left: scale(.03)},
-    "monitor_svg");
-
-var monitor_chart = monitor_svg.chart("Monitoring").eventFunc(function(){t = eval(monitor_data.eventFunction); return t;}).startFunc(monitor_data.startFunction)
-						.range(monitor_data.range).tick(monitor_data.ticks);
-
-if(!monitor_data.distractor) {
-    monitor_chart.when("alert", function(args){data.push({time: (new Date()).getTime()-startTime, eventType: "alert", chart: "monitoring", arg: args.args, id: args.domID, table: "Event"})});
-    monitor_chart.when("timeout", function(args){data.push({time: (new Date()).getTime()-startTime, eventType: "timeout", chart: "monitoring", arg: "", id: args.domID, table: "Event"})});
-    monitor_chart.when("response", function(args){data.push({time: args.time-startTime, eventType: "input", chart: "monitoring", arg: "correct:"+args.correct+",ascii: "+args.ascii, id: args.domID, table: "Event"})});
-}
-
-monitor_chart.draw(monitor_data);
-
-keyboard.push(function(e, time){
-    if(!monitor_data.distractor){
-    	monitor_data.scales.forEach(function(d,i){
-            if(d.button == e.keyCode){
-                monitor_chart.resetSliders(i, time);
-            }
-        });
-
-        monitor_data.buttons.forEach(function(d,i){
-            if(d.button == e.keyCode){
-                monitor_chart.resetButtons(i, time);
-            }
-        });
+(function() {
+    var monitor_data = setup.Monitoring.data;
+    
+    var monitor_param = monitor_data.parameters;
+    
+    monitor_svg = GUIUtil.getGenericSVG(
+        d3.select("#"+setup.Monitoring.container),
+        650, 650,
+        JSON.parse(document.getElementById(setup.Monitoring.container).style.width.substr(0,document.getElementById(setup.Monitoring.container).style.width.length-2)), 
+        JSON.parse(document.getElementById(setup.Monitoring.container).style.height.substr(0,document.getElementById(setup.Monitoring.container).style.height.length-2)),
+        {top: scale(.03), right: scale(.03), bottom: scale(.03), left: scale(.03)},
+        "monitor_svg");
+    
+    var monitor_chart = monitor_svg.chart("Monitoring").eventFunc(function(){t = eval(monitor_data.eventFunction); return t;}).startFunc(monitor_data.startFunction)
+    						.range(monitor_data.range).tick(monitor_data.ticks);
+    
+    if(!monitor_data.distractor) {
+        monitor_chart.when("alert", function(args){data.push({time: (new Date()).getTime()-startTime, eventType: "alert", chart: "monitoring", arg: args.args, id: args.domID, table: "Event"})});
+        monitor_chart.when("timeout", function(args){data.push({time: (new Date()).getTime()-startTime, eventType: "timeout", chart: "monitoring", arg: "", id: args.domID, table: "Event"})});
+        monitor_chart.when("response", function(args){data.push({time: args.time-startTime, eventType: "input", chart: "monitoring", arg: "correct:"+args.correct+",ascii: "+args.ascii, id: args.domID, table: "Event"})});
     }
-});
+    
+    monitor_chart.draw(monitor_data);
+    
+    keyboard.push(function(e, time){
+        if(!monitor_data.distractor){
+        	monitor_data.scales.forEach(function(d,i){
+                if(d.button == e.keyCode){
+                    monitor_chart.resetSliders(i, time);
+                }
+            });
+    
+            monitor_data.buttons.forEach(function(d,i){
+                if(d.button == e.keyCode){
+                    monitor_chart.resetButtons(i, time);
+                }
+            });
+        }
+    });
+})();
--- static/ModME/resource.js
+++ static/ModME/resource.js
@@ -1,54 +1,57 @@
-var resource_data = setup.Resource.data;
-var resource_param = resource_data.parameters;
+(function() {
+    var resource_data = setup.Resource.data;
 
-    var resource_svg = GUIUtil.getGenericSVG(
-        d3.select("#"+setup.Resource.container),
-        1300, 650,
-        JSON.parse(document.getElementById(setup.Resource.container).style.width.substr(0,document.getElementById(setup.Resource.container).style.width.length-2)), 
-        JSON.parse(document.getElementById(setup.Resource.container).style.height.substr(0,document.getElementById(setup.Resource.container).style.height.length-2)),
-        {top: scale(.03), right: scale(.03), bottom: scale(.03), left: scale(.03)},
-        "resource_svg");
-
-    resource_chart = resource_svg.chart("Resource").eventFunc(function(){t = eval(resource_data.eventFunction); return t;})
-        .refreshRate(resource_data.refresh).startFunc(resource_data.startFunction);
-
-    resource_data.switches.forEach(function(d){
-            if(d.source < 100){
-                d.source = resource_data.tanks[d.source];
-            }
-            else{
-                d.source = resource_data.generators[d.source-100];
-            }
-            if(d.target<100){
-                d.target = resource_data.tanks[d.target];
-            }
-            else{
-                d.target = resource_data.generators[d.source-100];
-            }
-        });
-
-if(!resource_data.distractor) {
-    resource_chart.when("alert", function(args){data.push({time: (new Date()).getTime()-startTime, eventType: "alert", chart: "resource", arg: "", id: args.domID, table: "Event"})});
-    resource_chart.when("tick", function(args){
-                                            resource_data.tanks.forEach(function(d,i){
-                                                data.push({time: (new Date()).getTime()-startTime, tankNumber: i+1, state: args.tanks[i].resource, table: "Tank"});
-                                            })
-                                            resource_data.switches.forEach(function(d,i){
-                                                data.push({time: (new Date()).getTime()-startTime, switchNumber: i+1, state: args.state[i], table: "Switch"});
-                                            })
-                                        });
-    resource_chart.when("response", function(args){data.push({time: args.time-startTime, eventType: "input", chart: "resource", arg: args.ascii, id: args.domID, table: "Event"})});
-    resource_chart.when("timeout", function(args){});
-}
-
-resource_chart.draw(resource_data);
-
-keyboard.push(function(e, time){
+    var resource_param = resource_data.parameters;
+    
+        var resource_svg = GUIUtil.getGenericSVG(
+            d3.select("#"+setup.Resource.container),
+            1300, 650,
+            JSON.parse(document.getElementById(setup.Resource.container).style.width.substr(0,document.getElementById(setup.Resource.container).style.width.length-2)), 
+            JSON.parse(document.getElementById(setup.Resource.container).style.height.substr(0,document.getElementById(setup.Resource.container).style.height.length-2)),
+            {top: scale(.03), right: scale(.03), bottom: scale(.03), left: scale(.03)},
+            "resource_svg");
+    
+        resource_chart = resource_svg.chart("Resource").eventFunc(function(){t = eval(resource_data.eventFunction); return t;})
+            .refreshRate(resource_data.refresh).startFunc(resource_data.startFunction);
+    
+        resource_data.switches.forEach(function(d){
+                if(d.source < 100){
+                    d.source = resource_data.tanks[d.source];
+                }
+                else{
+                    d.source = resource_data.generators[d.source-100];
+                }
+                if(d.target<100){
+                    d.target = resource_data.tanks[d.target];
+                }
+                else{
+                    d.target = resource_data.generators[d.source-100];
+                }
+            });
+    
     if(!resource_data.distractor) {
-        resource_data.switches.forEach(function(d,i){
-            if(d.key == e.keyCode){
-                resource_chart.flipSwitch(i, time);
-            }
-        });
+        resource_chart.when("alert", function(args){data.push({time: (new Date()).getTime()-startTime, eventType: "alert", chart: "resource", arg: "", id: args.domID, table: "Event"})});
+        resource_chart.when("tick", function(args){
+                                                resource_data.tanks.forEach(function(d,i){
+                                                    data.push({time: (new Date()).getTime()-startTime, tankNumber: i+1, state: args.tanks[i].resource, table: "Tank"});
+                                                })
+                                                resource_data.switches.forEach(function(d,i){
+                                                    data.push({time: (new Date()).getTime()-startTime, switchNumber: i+1, state: args.state[i], table: "Switch"});
+                                                })
+                                            });
+        resource_chart.when("response", function(args){data.push({time: args.time-startTime, eventType: "input", chart: "resource", arg: args.ascii, id: args.domID, table: "Event"})});
+        resource_chart.when("timeout", function(args){});
     }
-});
+    
+    resource_chart.draw(resource_data);
+    
+    keyboard.push(function(e, time){
+        if(!resource_data.distractor) {
+            resource_data.switches.forEach(function(d,i){
+                if(d.key == e.keyCode){
+                    resource_chart.flipSwitch(i, time);
+                }
+            });
+        }
+    });
+})();
